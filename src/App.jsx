d44f8ab42148f952ba5a1539cccef70a3a20cc01
nabaraj82 <nabarajmagar54@gmail.com>
2022-09-16 18:11:05 +0545

    folder structure corrected

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,7 @@ import TypingEffect from './TypingEffect'
 const App = () => {
   return (
     <div>
-<<<<<<< HEAD
       <TypingEffect phrase="Hello World" />
-=======
-      <TypingEffect text="Heello World" />
->>>>>>> 6bdb51cbf1e1fc9d630a7c8b0fc5f0bd68eacc8f
     </div>
   )
 }
--- src/TypingEffect.jsx
+++ src/TypingEffect.jsx
@@ -1,27 +1,33 @@
-import React from 'react'
-import { useEffect } from 'react';
-import { useState,useRef } from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react'
 
-const TypingEffect = ({text}) => {
-    const [currentText, setCurrentText] = useState('');
-    useEffect(() => {
-        const timerID = setTimeout(() =>{
-          printText(text)
-        },100)
-        return () =>{
-          clearTimeout(timerID)
-        }
-    },[text,currentText])
-    const index = useRef(0)
-   function printText(text){
-      setCurrentText((value) => value + text.charAt(index.current))
-      index.current +=1
-      console.log(index.current)
-   }
+const TypingEffect = ({phrase}) => {
+  const [text, setText] = useState('')
+  const [forwardIndex, setForwardIndex] = useState(0)
+  const [backwardIndex, setBackwardIndex] = useState(0)
+  useEffect(()=>{
+    if(forwardIndex<phrase.length){
+
+      setTimeout(()=>{
+        setText(text+phrase[forwardIndex])
+      setForwardIndex(forwardIndex+1)
+      },200)
+    }else{
+        setBackwardIndex(phrase.length)
+    }
+  },[forwardIndex])
+  useEffect(()=>{
+    if(backwardIndex >= 0){
+      setTimeout(()=>{
+        setText(phrase.slice(0,backwardIndex))
+        setBackwardIndex(backwardIndex-1)
+      },100)
+    }else{
+      setForwardIndex(0)
+    }
+  },[backwardIndex])
   return (
-    <div>{
-        currentText
-    }</div>
+    <div>{text}</div>
   )
 }
 
